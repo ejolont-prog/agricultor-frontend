fa52a528c3c7e246be2c05c2855db57f68c39bd6
Hilda Aileen Raymundo Díaz <hraymundod@miumg.edu.gt>
2026-05-08 20:43:40 -0600

    config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http'; // 1. Importar esto
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { authInterceptor } from './auth/auth.interceptor'; // 2. Importar tu interceptor
+import { authInterceptor } from './auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync()]
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    // 3. Habilitar HttpClient con tu interceptor
+    // Configuramos el cliente HTTP con tu interceptor de autenticación
     provideHttpClient(
       withInterceptors([authInterceptor])
     )
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,16 +7,14 @@ import { TransportistaComponent } from './components/transportista/transportista
 import { NuevaparcialidadComponent } from './components/parcialidades/nuevaparcialidad/nuevaparcialidad.component';
 import { CrearTransportistaComponent } from './components/transportista/creartransportista/creartransportista.component';
 import { CrearTransporteComponent } from './components/transporte/crear-transporte/crear-transporte.component';
-
-// 1. Importa el guard (asegúrate de que la ruta del archivo sea correcta)
 import { authGuard } from './auth/auth.guard';
-import {CrearPesajeComponent} from "./components/pesajes/crear-pesaje/crear-pesaje.component";
+import { CrearPesajeComponent } from "./components/pesajes/crear-pesaje/crear-pesaje.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
-    canActivate: [authGuard] // Protege el dashboard
+    canActivate: [authGuard]
   },
   {
     path: 'parcialidades',
@@ -53,9 +51,12 @@ export const routes: Routes = [
     component: CrearTransportistaComponent,
     canActivate: [authGuard]
   },
-  { path: 'transporte/crear',
+  {
+    path: 'transporte/crear',
     component: CrearTransporteComponent,
-    canActivate: [authGuard] },
+    canActivate: [authGuard]
+  },
+  {
     path: '**',
     redirectTo: '',
     pathMatch: 'full'
