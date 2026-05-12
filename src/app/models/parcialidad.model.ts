dff785c1fb77c6815753b67eb1b8715ec21949db
Esau Jolon <ejolont@miumg.edu.gt>
2026-05-11 22:10:48 -0600

    placa

--- src/app/models/parcialidad.model.ts
+++ src/app/models/parcialidad.model.ts
@@ -3,13 +3,22 @@ export interface Parcialidad {
   idpesaje: number;
   idtransporte: number;
   idtransportista: number;
+
+  // Agrega estos dos objetos para que el HTML los reconozca
+  transporte?: {
+    idtransporte: number;
+    placa: string;
+  };
+  transportista?: {
+    idtransportista: number;
+    nombreCompleto: string;
+    cui: string;
+  };
+
   pesoestimadoparcialidad: number;
   estadoparcialidad: number;
   textorechazado?: string;
   tipomedida?: string;
-  qr?: string;
-  faltante?: number;
-  sobrante?: number;
-  fechacreacion?: string | Date; // <--- Agrega esta línea
+  fechacreacion?: string | Date;
   eliminado?: boolean;
 }
